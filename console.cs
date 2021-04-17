0599ba51acdcf1dc90520ca321dbe743bb57996e
innominata <inno@xray.cat>
2021-04-17 17:25:36 +1200

    Initial Commit

---
+++ console.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BepInEx;
+
+namespace BCE
+{
+    [BepInPlugin("space.customizing.console", "Console Extensions", "1.0.0.0")]
+    public class console : BaseUnityPlugin 
+    {
+        public static void WriteLine(string text, ConsoleColor consoleColor)
+        {
+            ConsoleManager.SetConsoleColor(consoleColor);
+            ConsoleManager.StandardOutStream.WriteLine(text);
+        }
+        public static void Write(string text, ConsoleColor consoleColor)
+        {
+            ConsoleManager.SetConsoleColor(consoleColor);
+            ConsoleManager.StandardOutStream.Write(text);
+        }
+    }
+}
+
+
